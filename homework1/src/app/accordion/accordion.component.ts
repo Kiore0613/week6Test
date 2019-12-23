777c6d492ee46ff7ec52a6e21aa4ee7fd72f0a1b
Kiore <ricardochicas95@gmail.com>
2019-12-23 12:04:16 -0600

    Add mixins file and variables.scss

--- homework1/src/app/accordion/accordion.component.ts
+++ homework1/src/app/accordion/accordion.component.ts
@@ -7,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccordionComponent implements OnInit {
 
-  isOpen: boolean = true;
+  isOpen: boolean = false;
+  imageUrlLog = 'https://logos-coches.com/wp-content/uploads/2018/04/Bugatti-logo.png';
+  imageUrlCar = 'https://c.ndtvimg.com/2019-08/k8519lf8_bugatti-centodieci-unveiled-at-pebble-beach-car-show_625x300_17_August_19.jpg';
 
   constructor() { }
 
